3b7f53bbc1f619ef47a81638b41ec4cad3a31f75
miftah <miftah.ramadhan@noble-software.com>
2025-05-03 21:13:38 +0700

    update jsonfiletest and parseJsonFile

--- parseJsonFile.py
+++ parseJsonFile.py
@@ -18,3 +18,10 @@ print(f'value dari class adalah {jsonData['data']["class"]}')
 #print section : null value json --> in python terms null is None
 print(f'value dari class adalah {jsonData['data']["section"]}')
 
+#print value dari point berupa number desimal.
+print(f'value dari point adalah {jsonData['data']["dashboard"]["point"]}')
+print(type(jsonData['data']["dashboard"]["point"]))
+
+#print value dari age berupa number bilangan bulat/int.
+print(f'value dari age adalah {jsonData['data']["age"]}')
+print(type(jsonData['data']["age"]))
